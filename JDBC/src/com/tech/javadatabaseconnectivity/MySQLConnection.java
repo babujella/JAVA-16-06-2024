0100cd325479151bad87aaf8a57204b88659aa21
babu j <babu.j@techouts.com>
2024-06-16 23:35:45 +0530

    JDBC project files

---
+++ JDBC/src/com/tech/javadatabaseconnectivity/MySQLConnection.java
@@ -0,0 +1,12 @@
+package com.tech.javadatabaseconnectivity;
+
+import java.sql.Connection;
+
+public class MySQLConnection {
+	public static void main(String[] args) {
+		Class.forName(null)
+		
+
+	}
+
+}
